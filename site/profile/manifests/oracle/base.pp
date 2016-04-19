e8eb0c3be73a761c3c5fb91698e5074098e68612
Catalin Trifu <ctrifu@gmail.com>
2016-04-19 21:45:51 +0300

    auto-commit

--- site/profile/manifests/oracle/base.pp
+++ site/profile/manifests/oracle/base.pp
@@ -8,6 +8,7 @@ class profile::oracle::base {
     group   => 'oinstall',
     mode    => '0664',
     content => file('profile/oracle/bashrc'),
+    require => User['oracle'],
   }
   file { '/home/grid/.bashrc':
     ensure  => present,
@@ -15,6 +16,7 @@ class profile::oracle::base {
     group   => 'oinstall',
     mode    => '0664',
     content => file('profile/oracle/bashrc'),
+    require => User['grid'],
   }
   file { '/etc/udev/rules.d/99-asm.rules':
     ensure  => present,
@@ -22,5 +24,21 @@ class profile::oracle::base {
     group   => 'root',
     mode    => '0664',
     content => file('profile/etc/udev/rules.d/99-asm.rules'),
+  }
+  file { '/home/oracle/.ssh/config':
+    ensure  => present,
+    owner   => 'oracle',
+    group   => 'oinstall',
+    mode    => '0600',
+    content => file('profile/oracle/sshconfig'),
+    require => User['oracle'],
+  }
+  file { '/home/grid/.ssh/config':
+    ensure  => present,
+    owner   => 'grid',
+    group   => 'oinstall',
+    mode    => '0600',
+    content => file('profile/oracle/sshconfig'),
+    require => User['grid'],
   }
 }
