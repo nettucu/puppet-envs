3c3ffd374b0ee825d01799b57405cc1354d744cd
Catalin Trifu <ctrifu@gmail.com>
2018-10-29 22:45:44 +0200

    auto-commit

---
+++ site/profile/manifests/oracle/restart121.pp
@@ -0,0 +1,30 @@
+include ::oradb
+class profile::oracle::restart121 {
+  #  include ::oradb
+
+  package { 'oracle-rdbms-server-12cR1-preinstall':
+    ensure  => latest,
+    require => User['oracle'],
+  }
+  file { ['/u01', '/u01/app', '/u01/app/grid', '/u01/app/grid/product']:
+    ensure  => directory,
+    owner   => 'grid',
+    group   => 'oinstall',
+    mode    => '0775',
+    require => User['grid'],
+  }
+  file { '/u01/app/oracle':
+    ensure  => directory,
+    owner   => 'oracle',
+    group   => 'oinstall',
+    mode    => '0775',
+    require => User['oracle'],
+  }
+  file { '/u01/app/oraInventory':
+    ensure  => directory,
+    owner   => 'grid',
+    group   => 'oinstall',
+    mode    => '0775',
+    require => User['grid'],
+  }
+}
---
+++ site/role/manifests/restart121.pp
@@ -0,0 +1,3 @@
+class role::restart121 inherits role::oraclebase {
+  include profile::oracle::restart121
+}
