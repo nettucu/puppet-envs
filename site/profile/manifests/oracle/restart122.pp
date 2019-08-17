b5987a9f1ded045c042388f099732f739e144e19
Catalin Trifu <ctrifu@gmail.com>
2019-08-17 19:34:33 +0300

    auto-commit

--- site/profile/manifests/oracle/restart122.pp
+++ site/profile/manifests/oracle/restart122.pp
@@ -40,4 +40,17 @@ class profile::oracle::restart122 {
     puppet_download_mnt_point => '/mnt/db/oracle/226-Linux-x86-64/software/release/12.2.0.1.0',
     remote_file   =>  false,
   }
+
+  oradb::opatchupgrade {'12.2.0.1_opatch_upgrade':
+    oracle_home  => '/u01/app/oracle/product/12.2.0.1/db_122',
+    patch_file   => 'p6880880_122010_Linux-x86-64.zip',
+    csi_number   => undef,
+    support_id   => undef,
+    opversion    => 12.2.0.1.17,
+    user         => 'oracle',
+    group        => 'oinstall',
+    download_dir => '/mnt/db/oracle/226-Linux-x86-64/software/oneoffs/12.2.0.1.0',
+    require      => Oradb::Installdb['12.2.0.1_Linux-x86-64'],
+    puppet_download_mnt_point => '/mnt/db/oracle/226-Linux-x86-64/software/oneoffs/12.2.0.1.0',
+  }
 }
