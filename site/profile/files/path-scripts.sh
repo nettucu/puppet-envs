08f023c084c40a48f0dc9d9e73f49fa287cc7bc2
Catalin Trifu <ctrifu@gmail.com>
2019-09-20 13:03:21 +0300

    stuff

--- site/profile/files/path-scripts.sh
+++ site/profile/files/path-scripts.sh
@@ -1,11 +1,13 @@
-for d in $( find -L /opt/scripts -type f -executable ! \( -path '**/.git/*' -o -path '**/conf/*' -o -path '**/cron*' -o -path '**/dlds*' -o -path '**/dtrace*' \) -printf '%h\n' | uniq ); do
-    case ":${PATH}:" in
-        *:"$1":*)
-          ;;
-        *)
-          PATH="${PATH}:${d}"
-          ;;
-      esac
-done
+if [[ $- == *i* ]]; then
+    for d in $( find -L /opt/scripts -type f -executable ! \( -path '**/.git/*' -o -path '**/conf/*' -o -path '**/cron*' -o -path '**/dlds*' -o -path '**/dtrace*' \) -printf '%h\n' | uniq ); do
+      case ":${PATH}:" in
+          *:"$1":*)
+            ;;
+          *)
+            PATH="${PATH}:${d}"
+            ;;
+        esac
+    done
 
-export PATH
+    export PATH
+fi
