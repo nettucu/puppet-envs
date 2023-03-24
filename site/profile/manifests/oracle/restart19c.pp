1deaf93820ce00cbbcdf0beb5c4cd927f498e419
Catalin Trifu <ctrifu@gmail.com>
2023-03-24 20:01:36 +0200

    added options for 19c restart

---
+++ site/profile/manifests/oracle/restart19c.pp
@@ -0,0 +1,30 @@
+#include ::oradb
+class profile::oracle::restart18c {
+  include ::oradb
+
+  package { 'oracle-database-preinstall-19c':
+    ensure  => latest,
+    require => User['oracle'],
+  }
+  file { ['/u01', '/u01/app', '/u01/app/grid', '/u01/app/grid/product']:
+    ensure  => directory,
+    owner   => 'grid',
+    group   => 'oinstall',
+    mode    => '0775',
+    require => User['grid'],
+  }
+  file { ['/u01/app/oracle', '/u01/app/oracle/product']:
+    ensure  => directory,
+    owner   => 'oracle',
+    group   => 'oinstall',
+    mode    => '0775',
+    require => User['oracle'],
+  }
+  file { '/u01/app/oraInventory':
+    ensure  => directory,
+    owner   => 'grid',
+    group   => 'oinstall',
+    mode    => '0775',
+    require => User['grid'],
+  }
+}
