2e17049a6a8c289dd62b36b47c1d254b6808909f
Catalin Trifu <ctrifu@gmail.com>
2023-03-24 21:01:03 +0200

    auto-commit

--- site/profile/manifests/base_linux.pp
+++ site/profile/manifests/base_linux.pp
@@ -1,5 +1,5 @@
 class profile::base_linux {
-  if($::operatingsystemmajrelease < 8) {
+  if(versioncmp( $::operatingsystemmajrelease, 8) == -1) {
     class { '::ntp': }
   } else {
     class { '::chrony': }
